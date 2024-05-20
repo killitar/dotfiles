94640a52a00bf560954477c281dad9fcb51f87ad
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-05-20 18:38:29 +0300

    feat(plugins)!: use native comments on 0.10, with support for ts_context_commentstring

---
+++ nvim/lua/plugins/context_comment.lua
@@ -0,0 +1,17 @@
+return {
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	opts = {
+		enable_autocmd = false,
+	},
+	init = function()
+		vim.schedule(function()
+			local get_option = vim.filetype.get_option
+
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+						and require("ts_context_commentstring.internal").calculate_commentstring()
+					or get_option(filetype, option)
+			end
+		end)
+	end,
+}
