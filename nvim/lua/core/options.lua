5ec27148054e013ccc56eb1154fed7af4629db2d
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-06-01 10:20:24 +0300

    feat(core): refactor wsl clipboard function

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -90,15 +90,15 @@ vim.g.markdown_recommended_style = 0
 --clipboard wsl
 if vim.fn.has("wsl") == 1 then
 	vim.g.clipboard = {
-		name = "win32yank-wsl",
+		name = "WslClipboard",
 		copy = {
 			["+"] = "clip.exe",
 			["*"] = "clip.exe",
 		},
 		paste = {
-			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["+"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 		},
-		cache_enable = 0,
+		cache_enabled = 0,
 	}
 end
