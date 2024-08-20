755965cb5c02fed24bb8d4d39d1b33fd6d2e3c0e
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-08-20 15:05:41 +0300

    refactor(plugins): refactor mini-icons

--- nvim/lua/plugins/ui/mini-icons.lua
+++ nvim/lua/plugins/ui/mini-icons.lua
@@ -1,13 +1,16 @@
 return {
 	"echasnovski/mini.icons",
-	opts = {},
-	specs = {
-		{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+	opts = {
+		file = {
+			[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+			["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+		},
+		filetype = {
+			dotenv = { glyph = "", hl = "MiniIconsYellow" },
+		},
 	},
 	init = function()
 		package.preload["nvim-web-devicons"] = function()
-			-- needed since it will be false when loading and mini will fail
-			package.loaded["nvim-web-devicons"] = {}
 			require("mini.icons").mock_nvim_web_devicons()
 			return package.loaded["nvim-web-devicons"]
 		end
