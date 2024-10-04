5e8ceb1967f34256f043edccba74480fe525f194
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-10-04 12:59:52 +0300

    feat(plugins): refactor heirline config

--- nvim/lua/plugins/ui/heirline.lua
+++ nvim/lua/plugins/ui/heirline.lua
@@ -1,18 +1,19 @@
 return {
-	"rebelot/heirline.nvim",
-	event = "UIEnter",
-	config = function()
-		local colors = require("obscure.palettes").get_palette("obscure")
-		local icons = {
-			diagnostics = require("helpers.icons").get("diagnostics", true),
-			git = require("helpers.icons").get("git", true),
-		}
-		local separator = { left = "", right = "" }
+  "rebelot/heirline.nvim",
+  event = "UIEnter",
+  config = function()
+    local colors = require("obscure.palettes").get_palette("obscure")
+    local icons = {
+      diagnostics = require("helpers.icons").get("diagnostics", true),
+      git = require("helpers.icons").get("git", true),
+      ui = require("helpers.icons").get("ui", true),
+    }
+    local separator_statusline = { left = "", right = "" }
 
-		local StatusLines = require("plugins.ui.heirline_config.StatusLines").get(colors, separator, icons)
+    local StatusLines = require("plugins.ui.heirline_config.StatusLines").get(colors, separator_statusline, icons)
 
-		require("heirline").setup({
-			statusline = StatusLines,
-		})
-	end,
+    require("heirline").setup({
+      statusline = StatusLines,
+    })
+  end,
 }
