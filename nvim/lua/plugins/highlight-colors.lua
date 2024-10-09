70876e4d5307bbcdcd54a81f9e7c640691afff9d
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-10-09 15:47:19 +0300

    feat(highlight-colors): refactor config

--- nvim/lua/plugins/highlight-colors.lua
+++ nvim/lua/plugins/highlight-colors.lua
@@ -1,14 +1,16 @@
 return {
-	"brenoprata10/nvim-highlight-colors",
-	event = "BufReadPre",
-	opts = {
-		enable_tailwind = true,
-		exclude_filetypes = {
-			"Telescope",
-			"lazy",
-			"mason",
-			"lazyterm",
-			"toggleterm",
-		},
-	},
+  "brenoprata10/nvim-highlight-colors",
+  event = "BufReadPre",
+  opts = {
+    enable_tailwind = true,
+    render = "virtual",
+    virtual_symbol = "󱓻",
+    exclude_filetypes = {
+      "lazy",
+      "mason",
+      "lazyterm",
+      "toggleterm",
+      "TelescopeResults",
+    },
+  },
 }
