3fca0ae0b1fd84be76627a097d87c5c93a0b64d9
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-10-09 15:46:58 +0300

    feat(markdown): add new plugin markview.nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,7 +1,16 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		build = ":call mkdp#util#install()",
-	},
+  "OXY2DEV/markview.nvim",
+  ft = { "markdown", "quarto", "rmd" },
+  keys = {
+    { "<leader>um", ft = { "markdown", "quarto", "rmd" }, "<cmd>Markview toggleAll<cr>", desc = "Render Markdown" },
+  },
+  config = function()
+    local presets = require("markview.presets")
+
+    require("markview").setup({
+      headings = presets.headings.glow,
+      checkboxes = presets.checkboxes.nerd,
+      code_blocks = { sign = false },
+    })
+  end,
 }
