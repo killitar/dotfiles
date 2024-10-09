73d3aa511f74ef0bfd765f22bbbc6fc45c3c800c
Miroslav Lincovsci <lincovsci02@gmail.com>
2024-10-09 15:48:29 +0300

    feat(conform): refactor config

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -1,46 +1,46 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	event = "BufWritePre",
-	cmd = "ConformInfo",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			python = { "isort", "black" },
-			-- Use a sub-list to run only the first available formatter
-			javascript = { { "eslint_d", "prettierd", "prettier" } },
-			javascriptreact = { { "eslint_d", "prettierd", "prettier" } },
-			typescript = { { "eslint_d", "prettierd", "prettier" } },
-			typescriptreact = { { "eslint_d", "prettierd", "prettier" } },
-			vue = { { "eslint_d", "prettierd", "prettier" } },
-			svelte = { { "eslint_d", "prettierd", "prettier" } },
-			astro = { { "eslint_d", "prettierd", "prettier" } },
-			css = { { "prettierd", "prettier" } },
-			scss = { { "prettierd", "prettier" } },
-			less = { { "prettierd", "prettier" } },
-			html = { { "eslint_d", "prettierd", "prettier" } },
-			json = { { "eslint_d", "prettierd", "prettier" } },
-			jsonc = { { "eslint_d", "prettierd", "prettier" } },
-			yaml = { { "eslint_d", "prettierd", "prettier" } },
-			markdown = { { "eslint_d", "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
-			graphql = { { "eslint_d", "prettierd", "prettier" } },
-			handlers = { { "prettierd", "prettier" } },
+  "stevearc/conform.nvim",
+  event = "BufWritePre",
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "n",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black", stop_after_first = true },
+      javascript = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      typescript = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      vue = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      svelte = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      astro = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      scss = { "prettierd", "prettier", stop_after_first = true },
+      less = { "prettierd", "prettier", stop_after_first = true },
+      html = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
+      jsonc = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier", "markdownlint", "markdown-toc", stop_after_first = true },
+      graphql = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+      handlers = { "prettierd", "prettier", stop_after_first = true },
 
-			-- Use the "_" filetype to run formatters on filetypes that don't
-			-- have other formatters configured.
-			["_"] = { "trim_whitespace" },
-		},
-		-- format_on_save = {
-		--   -- These options will be passed to conform.format()
-		--   timeout_ms = 500,
-		--   lsp_fallback = true,
-		-- },
-		format_after_save = {
-			lsp_fallback = true,
-		},
+      ["_"] = { "trim_whitespace" },
+    },
+    format_after_save = {
+      lsp_format = "fallback",
+    },
 
-		log_level = vim.log.levels.ERROR,
-		notify_on_error = true,
-	},
+    log_level = vim.log.levels.ERROR,
+    notify_on_error = true,
+  },
 }
